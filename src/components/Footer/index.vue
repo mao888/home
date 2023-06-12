0aa6500e52bdfa62d53a495fab701a2d0fb20f35
geekswg <geekswg@qq.com>
2023-06-12 16:43:38 +0800

    📝 update

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -3,7 +3,7 @@
     <div class="power" v-show="!store.playerState">
       <span
         >Copyright&nbsp;&copy;&nbsp;{{ fullYear }}
-        <a href="https://imsyy.top">無名</a>
+        <a href="https://geekswg.js.cool">毕少侠</a>
       </span>
       <!-- 以下信息请不要修改哦 -->
       <span class="hidden"
@@ -14,10 +14,10 @@
           {{ config.author }}
         </a></span
       >&nbsp;&amp;
-      <!-- 站点备案 -->
-      <a href="https://beian.miit.gov.cn" target="_blank"
+      <!-- 站点备案 
+      <a href="https://beian.miit.gov.cn" target="_blank" class="hidden"
         >豫ICP备2022018134号-1</a
-      >
+      > -->
     </div>
     <div class="lrc" v-show="store.playerState">
       <music-one theme="filled" size="18" fill="#efefef" />
